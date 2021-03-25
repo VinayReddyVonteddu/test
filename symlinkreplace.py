e8912daefe8af0a42fafffd6795a3b34b07cd97e
vrvonteddu <vinay.reddy3989@gmail.com>
2021-03-25 11:27:18 -0400

    Used path.join to construct path, used tag_index to access tag, used Path.resolve() to verify symlink and used logging

--- symlinkreplace.py
+++ symlinkreplace.py
@@ -7,43 +7,44 @@ import pynmrstar
 import json
 import os
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 metabolomics = "/projects/BMRB/public/entry_directories/metabolomics/"
 for entryid in os.listdir(metabolomics):
     #Concatenate the path with entry ID(ex:bsme000xxxx)
-    eid_path = metabolomics + entryid
-    eid_file  = eid_path + "/" + entryid + ".str"
-
+    eid_path = os.path.join(metabolomics, entryid)
+    eid_file  = os.path.join(eid_path, entryid + ".str")
     #Verify if the path exists
     if not os.path.isdir(eid_path) or not os.path.exists(eid_file):
-        continue    
-    
-    entry = pynmrstar.Entry.from_file(eid_file)
-    
-    # Read the experiment file from the entries
-    files = entry.get_loops_by_category("_Experiment_file")
-    for fl in files:
-        dir_paths = fl.get_tag("Directory_path")
-        file_paths = fl.get_tag("Name")
-        data = json.loads(fl.get_json())
-        nameIndex = data['tags'].index('Name')
-        directoryIndex = data['tags'].index('Directory_path')
+        continue
 
-        for dir_path, file_path, dt in zip(dir_paths, file_paths, data['data']):
-            #concatenate the path for each directory and file path
-            path = eid_path + "/" + dir_path + file_path
-            #condition to check if path exists and its symlink
-            if os.path.exists(path) and os.path.islink(path):
-                print ("SYM LINK FOUND ", path)
-                # Assign the original link to the path_link 
-                path_link = Path(path).resolve()
+    entry = pynmrstar.Entry.from_file(eid_file)
+
+    files = entry.get_loops_by_category("_Experiment_file")
+    # Read the experiment file from the entries
+    for fl in files:
+        nameIndex = fl.tag_index("Name")
+        directoryIndex = fl.tag_index("Directory_path")
+
+        for dt in fl.data:
+            dir_path = dt[directoryIndex]
+            file_path = dt[nameIndex]
+            path = os.path.join(eid_path, dir_path, file_path)
+            #Assign the original link to path_link
+            path_link = Path(path).resolve()
+            path_link_uri = path_link.as_uri().replace("file://", "")
+            #condition to check if path is symlink
+            if path_link_uri != path:
+                logging.info("SYM LINK FOUND %s - > %s ", path, path_link_uri)
                 # Assign the parent of the path_link to the dir_link as URI which is after /entry id/ 
                 dir_link = path_link.parent.as_uri().split(entryid + "/")[1] + "/"
-                print ("SYM LINK RESOLVED ", dir_link, path_link.name)
+                logging.info("SYM LINK RESOLVED %s, %s", dir_link, path_link.name)
                 # Assign name and directory path
                 dt[nameIndex] = path_link.name
                 dt[directoryIndex] = dir_link
-        fl.data = data['data']
         fl.validate()
     link_path = "./link_directories/" + entryid + "/"
     os.makedirs(link_path)
     entry.write_to_file(link_path + entryid + ".str")
+
